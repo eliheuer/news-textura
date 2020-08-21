e9dee0125bfc119266eb5347e371158d967f898f
Eli Heuer <elih@protonmail.com>
2020-08-21 02:05:46 -0700

    Add BASH build script

---
+++ sources/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+fontmake -g sources/NewspaperText.glyphs -o ttf
+rm -rf instance_ufo
+mkdir -p fonts/ttf
+cp -r master_ufo/*.designspace sources/
+cp -r master_ufo/*.ufo sources/
+rm -rf master_ufo
+cp -r master_ttf/*.ttf fonts/ttf/
+rm -rf master_ttf
+
+#python3 documentation/instagram/basic-specimen.py
